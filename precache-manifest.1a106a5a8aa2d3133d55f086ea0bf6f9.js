e5d5047fbbdcb6dda3bbfff29605528cac478f8a
Nilesh <nileshjaat341@gmail.com>
2020-06-18 15:31:34 +0530

    Updates

---
+++ precache-manifest.1a106a5a8aa2d3133d55f086ea0bf6f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9400f150fffd32f1a2e439a4f28fe1c",
+    "url": "/io/todo/index.html"
+  },
+  {
+    "revision": "8e82f750787b8544d4d0",
+    "url": "/io/todo/static/css/main.72ce3b7b.chunk.css"
+  },
+  {
+    "revision": "2ef16eb83a9cd79fae25",
+    "url": "/io/todo/static/js/2.d188e0b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/io/todo/static/js/2.d188e0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e82f750787b8544d4d0",
+    "url": "/io/todo/static/js/main.e45d1506.chunk.js"
+  },
+  {
+    "revision": "1b410a795534db09c7f2",
+    "url": "/io/todo/static/js/runtime-main.8560812c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/io/todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
